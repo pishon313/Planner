ba8d426cc6fa55434c5f2c24b84d92d45a773427
pishon313 <pishon313@gmail.com>
2023-02-14 22:31:58 +0900

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by Sarah Jeong on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
